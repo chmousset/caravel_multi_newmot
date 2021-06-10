3558a7e57754af2fe838e0ae9897dca1fa145896
chmousset <ch.mousset@gmail.com>
2021-06-10 14:39:32 +0200

    Initial commit

---
+++ caravel_test/project.c
@@ -0,0 +1,90 @@
+/*
+ * SPDX-FileCopyrightText: 2020 Efabless Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "verilog/dv/caravel/defs.h"
+#include "csr.h"
+
+/*
+	IO Test:
+		- Configures MPRJ lower 8-IO pins as outputs
+		- Observes counter value through the MPRJ lower 8 IO pins (in the testbench)
+*/
+
+void main()
+{
+	/* 
+	IO Control Registers
+	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
+	| 3-bits | 1-bit | 1-bit | 1-bit  | 1-bit  | 1-bit | 1-bit   | 1-bit   | 1-bit | 1-bit | 1-bit   |
+
+	Output: 0000_0110_0000_1110  (0x1808) = GPIO_MODE_USER_STD_OUTPUT
+	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
+	| 110    | 0     | 0     | 0      | 0      | 0     | 0       | 1       | 0     | 0     | 0       |
+	
+	 
+	Input: 0000_0001_0000_1111 (0x0402) = GPIO_MODE_USER_STD_INPUT_NOPULL
+	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
+	| 001    | 0     | 0     | 0      | 0      | 0     | 0       | 0       | 0     | 1     | 0       |
+
+	*/
+
+    // 1 input for input signal
+	reg_mprj_io_8 =   GPIO_MODE_USER_STD_INPUT_NOPULL;
+
+    // 1 output for segments, starting at 9
+	reg_mprj_io_9 =   GPIO_MODE_USER_STD_OUTPUT;
+
+    /* Apply configuration */
+    reg_mprj_xfer = 1;
+    while (reg_mprj_xfer == 1);
+
+    // activate the project by setting the 1st bit of 2nd bank of LA - depends on the project ID
+    reg_la1_iena = 0; // input enable off
+    reg_la1_oenb = 0; // output enable on
+    reg_la1_data = 1 << 1;
+
+    // Inside caravel, the GPIO are output only.
+    gpio_oe_write(0b11); // both GPIOs are outputs 
+    gpio_o_write(0);
+
+    // PWM test: configure to generate a square wave
+    pwm_period_write(400);
+    pwm_width_write(200);
+    pwm_enable_write(1);
+
+    // Stepper IO setup
+    axis_x_stepper_x_mode_write(0b01); // step/dir
+    axis_x_stepper_x_invert_step_write(0);
+    axis_x_stepper_x_invert_dir_write(0);
+
+    // Stepgen test: generate 10 step forward
+    while(!generator_ready_axis_x_ready_read()); // wait for the axis to be ready taking new command
+    gpio_o_gpio_0_write(1); // mark the beginning of the motion
+    generator_axis_x_target_position_write(10);
+    generator_axis_x_start_speed_write(0);
+    generator_axis_x_acceleration_write(100);
+    generator_push_motion_write(1);
+
+    // Stepgen test: generate 10 step backwards
+    while(!generator_ready_axis_x_ready_read()); // wait for the axis to be ready taking new command
+    generator_axis_x_target_position_write(0);
+    generator_axis_x_start_speed_write(0);
+    generator_axis_x_acceleration_write(-100);
+    generator_push_motion_write(1);
+    while(!generator_home_done_axis_x_home_done_read()); // wait for all motion completion
+    gpio_o_gpio_1_write(1); // mark the end of the motion
+}
---
+++ wrapper.v
@@ -0,0 +1,132 @@
+`default_nettype none
+`ifdef FORMAL
+    `define MPRJ_IO_PADS 38    
+`endif
+// update this to the name of your module
+module wrapped_project(
+`ifdef USE_POWER_PINS
+    inout vdda1,	// User area 1 3.3V supply
+    inout vdda2,	// User area 2 3.3V supply
+    inout vssa1,	// User area 1 analog ground
+    inout vssa2,	// User area 2 analog ground
+    inout vccd1,	// User area 1 1.8V supply
+    inout vccd2,	// User area 2 1.8v supply
+    inout vssd1,	// User area 1 digital ground
+    inout vssd2,	// User area 2 digital ground
+`endif
+    // wishbone interface
+    input wire wb_clk_i,            // clock, runs at system clock
+    input wire wb_rst_i,            // main system reset
+    input wire wbs_stb_i,           // wishbone write strobe
+    input wire wbs_cyc_i,           // wishbone cycle
+    input wire wbs_we_i,            // wishbone write enable
+    input wire [3:0] wbs_sel_i,     // wishbone write word select
+    input wire [31:0] wbs_dat_i,    // wishbone data in
+    input wire [31:0] wbs_adr_i,    // wishbone address
+    output wire wbs_ack_o,          // wishbone ack
+    output wire [31:0] wbs_dat_o,   // wishbone data out
+
+    // Logic Analyzer Signals
+    // only provide first 32 bits to reduce wiring congestion
+    input  wire [31:0] la_data_in,  // from PicoRV32 to your project
+    output wire [31:0] la_data_out, // from your project to PicoRV32
+    input  wire [31:0] la_oenb,     // output enable bar (low for active)
+
+    // IOs
+    input  wire [`MPRJ_IO_PADS-1:0] io_in,  // in to your project
+    output wire [`MPRJ_IO_PADS-1:0] io_out, // out fro your project
+    output wire [`MPRJ_IO_PADS-1:0] io_oeb, // out enable bar (low active)
+
+    // IRQ
+    output wire [2:0] irq,          // interrupt from project to PicoRV32
+
+    // extra user clock
+    input wire user_clock2,
+    
+    // active input, only connect tristated outputs if this is high
+    input wire active
+);
+
+    // all outputs must be tristated before being passed onto the project
+    wire buf_wbs_ack_o;
+    wire [31:0] buf_wbs_dat_o;
+    wire [31:0] buf_la_data_out;
+    wire [`MPRJ_IO_PADS-1:0] buf_io_out;
+    wire [`MPRJ_IO_PADS-1:0] buf_io_oeb;
+    wire [2:0] buf_irq;
+
+    `ifdef FORMAL
+    // formal can't deal with z, so set all outputs to 0 if not active
+    assign wbs_ack_o    = active ? buf_wbs_ack_o    : 1'b0;
+    assign wbs_dat_o    = active ? buf_wbs_dat_o    : 32'b0;
+    assign la_data_out  = active ? buf_la_data_out  : 32'b0;
+    assign io_out       = active ? buf_io_out       : {`MPRJ_IO_PADS{1'b0}};
+    assign io_oeb       = active ? buf_io_oeb       : {`MPRJ_IO_PADS{1'b0}};
+    assign irq          = active ? buf_irq          : 3'b0;
+    `include "properties.v"
+    `else
+    // tristate buffers
+    assign wbs_ack_o    = active ? buf_wbs_ack_o    : 1'bz;
+    assign wbs_dat_o    = active ? buf_wbs_dat_o    : 32'bz;
+    assign la_data_out  = active ? buf_la_data_out  : 32'bz;
+    assign io_out       = active ? buf_io_out       : {`MPRJ_IO_PADS{1'bz}};
+    assign io_oeb       = active ? buf_io_oeb       : {`MPRJ_IO_PADS{1'bz}};
+    assign irq          = active ? buf_irq          : 3'bz;
+    `endif
+
+    assign buf_io_oeb[`MPRJ_IO_PADS-1: `io_pwm +1] = {(`MPRJ_IO_PADS){`INPUT}};// default all inputs
+    assign buf_io_oeb[`io_spi_miso] = `INPUT;
+    assign buf_io_oeb[`io_spi_clk] = `OUTPUT;
+    assign buf_io_oeb[`io_spi_cs] = `OUTPUT;
+    assign buf_io_oeb[`io_spi_mosi] = `OUTPUT;
+    assign buf_io_oeb[`io_pwm_native] = `OUTPUT;
+    assign buf_io_oeb[`io_ub_tx] = `OUTPUT;
+    assign buf_io_oeb[`io_ub_rx] = `INPUT;
+    assign buf_io_oeb[`io_gpio_gpio0] = `OUTPUT;
+    assign buf_io_oeb[`io_gpio_gpio1] = `OUTPUT;
+    assign buf_io_oeb[`io_x_step] = `OUTPUT;
+    assign buf_io_oeb[`io_x_dir] = `OUTPUT;
+    assign buf_io_oeb[`io_y_step] = `OUTPUT;
+    assign buf_io_oeb[`io_y_dir] = `OUTPUT;
+    assign buf_io_oeb[`io_pwm] = `OUTPUT;
+
+    top newmot (
+        `ifdef USE_POWER_PINS
+    /*  .vdda1(vdda1),  // User area 1 3.3V power
+        .vdda2(vdda2),  // User area 2 3.3V power
+        .vssa1(vssa1),  // User area 1 analog ground
+        .vssa2(vssa2),  // User area 2 analog ground
+        .vccd1(vccd1),  // User area 1 1.8V power
+        .vccd2(vccd2),  // User area 2 1.8V power
+        .vssd1(vssd1),  // User area 1 digital ground
+        .vssd2(vssd2),  // User area 2 digital ground*/
+        `endif
+
+        .sys_clk(wb_clk_i),
+        .sys_rst(wb_rst_i),
+
+        // MGMT SoC Wishbone Slave
+
+        .wb_cyc(wbs_cyc_i),
+        .wb_stb(wbs_stb_i),
+        .wb_we(wbs_we_i),
+        .wb_sel(wbs_sel_i),
+        .wb_adr(wbs_adr_i[14:2]),
+        .wb_dat_r(buf_wbs_dat_o),
+        .wb_dat_w(wbs_dat_i),
+        .wb_ack(buf_wbs_ack_o),
+
+        // IO Pads
+        .pwm(buf_io_out[`io_pwm_native]),
+        .gpio_gpio_0(buf_io_out[`io_gpio_gpio0]),
+        .gpio_gpio_1(buf_io_out[`io_gpio_gpio1]),
+        .stepper0_step(buf_io_out[`io_x_step]),
+        .stepper0_dir(buf_io_out[`io_x_dir]),
+        .uartbone_tx(buf_io_out[`io_ub_tx]),
+        .uartbone_rx(buf_io_out[`io_ub_rx]),
+        .pwm_out(buf_io_out[`io_pwm])
+    );
+
+
+endmodule 
+`default_nettype wire
